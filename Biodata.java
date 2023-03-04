5c5e376db6c96b984951f12b6edd06c112647a07
HadyanHanif <126949229+HadyanHanif@users.noreply.github.com>
2023-03-04 15:50:48 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,10 @@
+public class Biodata{
+
+    public static void main(String[] args){
+        String nama = "Ananda Hadyan Hanif";
+
+        System.out.println("Perkenalkan Nama Saya = "+nama);
+        System.out.println("Saya Merupakan Mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis Program JAVA");
+    }
+}
